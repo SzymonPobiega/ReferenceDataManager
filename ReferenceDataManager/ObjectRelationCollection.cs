42f6db602dbda9e81bee5f445f6d1b606dcd75b8
Szymon Pobiega <szymon@pobiega.com>
2012-01-13 21:31:26 +0100

    Object relation tests passing

--- ReferenceDataManager/ObjectRelationCollection.cs
+++ ReferenceDataManager/ObjectRelationCollection.cs
@@ -5,14 +5,31 @@ namespace ReferenceDataManager
 {
     public class ObjectRelationCollection
     {
+        private readonly Dictionary<string, List<Guid>> relations = new Dictionary<string, List<Guid>>();
+        private static readonly Guid[] emptyRelatedList = new Guid[] {};
+
         public void Attach(Guid refereeObjectId, string relationName)
         {
-            
+            List<Guid> existingRelation;
+            if (relations.TryGetValue(relationName, out existingRelation))
+            {
+                existingRelation.Add(refereeObjectId);
+            }
+            else
+            {
+                var newRelation = new List<Guid> {refereeObjectId};
+                relations[relationName] = newRelation;
+            }
         }
 
         public IEnumerable<Guid> GetRelated(string relationName)
         {
-            yield break;
+            List<Guid> existingRelation;
+            if (relations.TryGetValue(relationName, out existingRelation))
+            {
+                return existingRelation;
+            }
+            return emptyRelatedList;
         }
     }
 }
