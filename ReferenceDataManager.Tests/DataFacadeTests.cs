ac647c8581cf93d2f9f899b8557ee27f7a7555ae
Szymon Pobiega <szymon@pobiega.com>
2012-01-14 13:33:06 +0100

    Added guards for improper API usage on DataFacade

--- ReferenceDataManager.Tests/DataFacadeTests.cs
+++ ReferenceDataManager.Tests/DataFacadeTests.cs
@@ -9,6 +9,26 @@ namespace ReferenceDataManager.Tests
     public class DataFacadeTests
     {
         [Test]
+        public void It_throws_exception_when_trying_to_load_two_change_sets_with_same_ids()
+        {
+            var changeSetId = ChangeSetId.NewUniqueId();
+            var facade = new DataFacade();
+            facade.LoadChangeSet(new ChangeSet(changeSetId, null, new AbstractCommand[] {}));
+            
+            Assert.Throws<InvalidOperationException>( () => facade.LoadChangeSet(new ChangeSet(changeSetId, null, new AbstractCommand[] {})) );
+        }
+
+        [Test]
+        public void It_throws_exception_when_trying_to_load_object_in_context_of_non_existing_change_set()
+        {
+            var objectId = ObjectId.NewUniqueId();
+            var changeSetId = ChangeSetId.NewUniqueId();
+            var facade = new DataFacade();
+
+            Assert.Throws<InvalidOperationException>(() => facade.GetById(changeSetId, objectId));
+        }
+
+        [Test]
         public void It_creates_object_and_returns_it_by_id()
         {
             var changeSetId = ChangeSetId.NewUniqueId();
--- ReferenceDataManager/DataFacade.cs
+++ ReferenceDataManager/DataFacade.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ReferenceDataManager
 {
@@ -8,12 +9,20 @@ namespace ReferenceDataManager
 
         public ObjectState GetById(ChangeSetId changeSetId, ObjectId objectId)
         {
-            var snapshot = snapshots[changeSetId];
+            Snapshot snapshot;
+            if (!snapshots.TryGetValue(changeSetId, out snapshot))
+            {
+                throw new InvalidOperationException(string.Format("Cannot load state of object {0} in context of non-existing change set {1}", objectId, changeSetId));
+            }
             return snapshot.GetById(objectId);
         }
 
         public void LoadChangeSet(ChangeSet changeSet)
         {
+            if (snapshots.ContainsKey(changeSet.Id))
+            {
+                throw new InvalidOperationException(string.Format("Another change set with the id {0} has already been loaded.", changeSet.Id));
+            }
             snapshots[changeSet.Id] = CreateSnapshot(changeSet);
         }
 
