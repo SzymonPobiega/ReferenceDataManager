fddc3e410673ae8343f59aead8392f7eb7270f1f
Szymon Pobiega <szymon@pobiega.com>
2012-01-14 14:13:46 +0100

    Register type using reflection test passing

--- ReferenceDataManager/ObjectTypeDescriptorRepositoryExtensions.cs
+++ ReferenceDataManager/ObjectTypeDescriptorRepositoryExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace ReferenceDataManager
 {
@@ -6,6 +7,16 @@ namespace ReferenceDataManager
     {
         public static ObjectTypeDescriptorRepository RegisterUsingReflection(this ObjectTypeDescriptorRepository repository, Type typeToRegister)
         {
+            var typeAttibute = typeToRegister
+                .GetCustomAttributes(typeof (ObjectTypeAttribute), false)
+                .Cast<ObjectTypeAttribute>()
+                .Single();
+
+            var objectTypeId = new ObjectTypeId(new Guid(typeAttibute.TypeIdValue));
+            var typeDescritor = new ObjectTypeDescriptor(typeToRegister, objectTypeId, new AttributeDescriptor[] {},
+                                                         new RelationDescriptor[] {});
+
+            repository.RegisterTypeDescriptor(typeDescritor);
             return repository;
         }
     }
