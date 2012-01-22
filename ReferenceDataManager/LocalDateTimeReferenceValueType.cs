a605e1c986ef147e847ed09bc47111e4e9e11467
Szymon Pobiega <szymon@pobiega.com>
2012-01-22 07:11:16 +0100

    Added reference value type for local time

---
+++ ReferenceDataManager/LocalDateTimeReferenceValueType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReferenceDataManager
+{
+    public class LocalDateTimeReferenceValueType : IReferenceValueType
+    {
+        public IComparable GetCurrentValue()
+        {
+            return DateTime.Now;
+        }
+
+        public bool IsCompatible(IComparable value)
+        {
+            if (!(value is DateTime))
+            {
+                return false;
+            }
+            var dateValue = (DateTime) value;
+            return dateValue.Kind == DateTimeKind.Local;
+        }
+    }
+}
